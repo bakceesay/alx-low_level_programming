b5c3f81e0f6f55999fc7e9d8f5531ec4cd2e2327
bakceesay <liceesay24@gmail.com>
2022-10-19 01:29:42 -0700

    Task Twelve n times tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table.
+ * Return: 0.
+ */
+void print_times_table(int n)
+{
+	int i, j, rem;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				rem = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (rem < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (rem >= 10 && rem < 100)
+				{
+					_putchar(' ');
+					_putchar((rem / 10) + '0');
+					_putchar((rem % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((rem / 100) + '0');
+					_putchar((rem / 10) % 10 + '0');
+					_putchar((rem % 10) + '0');
+				}
+				else
+					_putchar((rem % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
