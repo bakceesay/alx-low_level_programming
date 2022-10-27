cc35b009999ac3336524e936eaeb55bb52f69a69
bakceesay <liceesay24@gmail.com>
2022-10-27 14:08:01 -0700

    Task nine a function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+* print_number - Prints an integer.
+* @n: The integer to be printed.
+*/
+
+void print_number(int n);
+{
+for (n = 1; n <= 100;	c++)
+_putchar("%d ", n);
+
+return (0);
+}
